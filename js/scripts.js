c903e480d5cede4d4df4e92e9b8f0e13ce4505a5
Alyssa DiCarlo <alyssa.dicarlo@gmail.com>
2021-05-26 10:21:44 -0400

    added updates

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,12 @@ const title = document.querySelector('#title');
 const subtitle = document.querySelector('.subtitle');
 const firstParagraph = document.querySelector('p');
 const allParagraphs = document.querySelectorAll('p');
+const clickMeButton = document.querySelector('#clickMe');
 
 const h3element = document.createElement('h3');
 h3element.innerText = "Hello, I am an h3 tag!";
 
 rootElement.append(h3element);
-
-title.innerText = "Hello JavaScript!";
 
 const h1element = document.querySelector('#heading');
 h1element.innerText = 'Toppings';
@@ -25,3 +24,16 @@ newListItem.innerText = 'Ketchup';
 const listItem = document.querySelector('#list');
 
 listItem.append(newListItem);
+
+let clickCount = 0;
+
+function clickCounter() {
+    clickCount = clickCount + 1;
+    console.log('click count is ', clickCount);
+    return clickCount;
+}
+
+clickMeButton.addEventListener('click', function () {
+    clickCounter();
+    title.innerText = "Hello JavaScript!";
+});
